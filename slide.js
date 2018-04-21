5a59e2737d8a50252d2799d64048162bd8672192
Nick Boncella <njb371@protonmail.com>
2018-04-21 05:24:16 -0400

    Add slide in functionality

--- slide.js
+++ slide.js
@@ -1,3 +1,4 @@
+/*** DEBOUNCE FUNCTION - prevent function from constantly firing ***/
 function debounce(func, wait = 20, immediate = true) {
   var timeout;
   return function() {
@@ -13,3 +14,22 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+/*** DOM query to select slide imgs ***/
+const images = document.querySelectorAll('.slide-in');
+
+/*** SLIDE IMAGES FUNCTION ***/
+function slideImages(e) {
+  images.forEach(image => {
+    // scroll through the img
+    const slideInAt = (window.scrollY + window.innerHeight) - (image.height / 4);
+    // bottom of the img
+    const imageBottom = image.offsetTop + image.height;
+    const isShown = slideInAt > image.offsetTop;
+    if (isShown) {
+      image.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(slideImages));
